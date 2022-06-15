d608b9ad0b0d687d8575680d0e6f082ee6b10ad9
Renato <accounts01@softplayers.com>
2022-06-15 16:26:58 -0300

    feat: category attributes and tsts

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,63 @@
-import {Category} from './category';
+import { Category } from "./category";
 
 describe("Category Tests", () => {
-    test("Contructor of category", () => {
-        const category = new Category("movie");
-        expect(category.name).toBe('movie')
+  test("Contructor of category", () => {
+    // Arrange
+    const now = new Date();
+    const fullProps = {
+      name: "movie",
+      description: "description",
+      is_active: true,
+      created_at: now,
+    };
+
+    // Minimal constructor
+    let category = new Category({ name: "Movie" });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: null,
+      is_active: true,
     });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    // Big Constructor
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    // Name + Desc
+    category = new Category({
+      name: "Movie 2",
+      description: "some description 2",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie 2",
+      description: "some description 2",
+      is_active: true,
+    });
+
+    // Name + Active
+    category = new Category({ name: "Movie 2", is_active: false });
+    expect(category.props).toMatchObject({
+      name: "Movie 2",
+      description: null,
+      is_active: false,
+    });
+
+    // Full Constructor
+    category = new Category(fullProps);
+    expect(category.name).toBe("movie");
+    expect(category.description).toBe("description");
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBe(now);
+    expect(category.props).toMatchObject(fullProps);
+  });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,38 @@
+export type CategoryProperties = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
 export class Category {
-    constructor(public name:string){}
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description(): string | null | undefined {
+    return this.props.description;
+  }
+
+  private set description(value: string | null | undefined) {
+    this.props.description = value ?? null;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  private set is_active(value: boolean | undefined) {
+    this.props.is_active = value ?? true;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
