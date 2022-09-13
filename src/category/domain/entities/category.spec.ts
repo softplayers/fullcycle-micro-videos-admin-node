eca1c8f879d0d52de563511b15675dff6bbe2603
Renato <accounts01@softplayers.com>
2022-09-13 12:52:33 -0300

    feat: cti

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,5 +1,5 @@
 import { Category } from "./category";
-import UniqueEntityId from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
+import UniqueEntityId from "#seedwork/domain/value-objects/unique-entity-id.vo";
 
 
 describe("Category Tests", () => {
