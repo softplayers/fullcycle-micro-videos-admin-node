86efd394d8bd60d35cff0ead4ff353079eb184f6
Renato <accounts01@softplayers.com>
2023-01-02 17:23:12 -0300

    chore: improvements on categories

--- src/nestjs/src/categories/categories.controller.spec.ts
+++ src/nestjs/src/categories/categories.controller.spec.ts
@@ -1,9 +1,7 @@
-import { CreateCategoryUseCase } from '@fc/micro-videos/category/application';
-import UniqueEntityId from '@fc/micro-videos/dist/@seedwork/domain/value-objects/unique-entity-id.vo';
-import { Test, TestingModule } from '@nestjs/testing';
+import { CreateCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase } from '@fc/micro-videos/category/application';
+import { SortDirection } from '@fc/micro-videos/dist/@seedwork/domain/repository/repository-contracts';
 import { CategoriesController } from './categories.controller';
-import { CategoriesService } from './categories.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 describe('CategoriesController Unit Tests', () => {
   let controller: CategoriesController;
@@ -25,40 +23,111 @@ describe('CategoriesController Unit Tests', () => {
 
     const expectedOutput: CreateCategoryUseCase.Output = {
       id: "ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729",
-      name: "Movie", 
-      description: "some description", 
+      name: "Movie",
+      description: "some description",
       is_active: true,
       created_at: new Date(),
     }
 
-    const mockCreateUseCase = {execute: jest.fn().mockReturnValue(expectedOutput)}
+    const mockCreateUseCase = { execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)) }
     // @ts-expect-error
     controller['createUseCase'] = mockCreateUseCase;
 
-    const input: CreateCategoryDto = {
-      name: "Movie", 
-      description: "some description", 
+    const input: UpdateCategoryDto = {
+      name: "Movie",
+      description: "some description",
       is_active: true
     };
 
-    const output = controller.create(input);
+    const output = await controller.create(input);
     expect(mockCreateUseCase.execute).toBeCalledWith(input);
     expect(output).toStrictEqual(expectedOutput);
   });
 
-  it('should update a category', () => {
-    expect(controller).toBeDefined();
+  it('should update a category', async () => {
+    const id = 'ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729';
+    const expectedOutput: UpdateCategoryUseCase.Output = {
+      id,
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: new Date(),
+    }
+
+    const mockUpdateUseCase = { execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)) }
+    // @ts-expect-error
+    controller['updateUseCase'] = mockUpdateUseCase;
+    const input: UpdateCategoryDto = {
+      name: "Movie",
+      description: "some description",
+      is_active: true
+    };
+
+    const output = await controller.update(id, input);
+    expect(mockUpdateUseCase.execute).toBeCalledWith({ id, ...input });
+    expect(output).toStrictEqual(expectedOutput);
   });
-  
+
   it('should delete a category', () => {
-    expect(controller).toBeDefined();
+    const expectedOutput = undefined;
+    const mockDeleteUseCase = { execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)) }
+    // @ts-expect-error
+    controller['deleteUseCase'] = mockDeleteUseCase;
+    const id = 'ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729';
+    expect(controller.remove(id)).toBeInstanceOf(Promise);
+    const output = controller.remove(id);
+    expect(mockDeleteUseCase.execute).toBeCalledWith({ id });
+    expect(output).toStrictEqual(output);
   });
 
-  it('should get a category', () => {
-    expect(controller).toBeDefined();
+  it('should get a category', async () => {
+    const id = 'ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729';
+    const expectedOutput: GetCategoryUseCase.Output = {
+      id: "ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729",
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: new Date(),
+    }
+    const mockGetUseCase = { execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)) }
+    // @ts-expect-error
+    controller['getUseCase'] = mockGetUseCase;
+    const output = controller.findOne(id);
+    expect(mockGetUseCase.execute).toBeCalledWith({ id });
+    expect(output).toStrictEqual(output);
   });
 
-  it('should list categories', () => {
-    expect(controller).toBeDefined();
+  it('should list categories', async () => {
+    const expectedOutput: ListCategoriesUseCase.Output = {
+      items: [
+        {
+          id: "ab4f61f2-ddc5-4bf9-a2e5-245d19bfd729",
+          name: "Movie",
+          description: "some description",
+          is_active: true,
+          created_at: new Date(),
+        }
+      ],
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+      total: 1
+    };
+
+    const mockListUseCase = { execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)) }
+    // @ts-expect-error
+    controller['listUseCase'] = mockListUseCase;
+
+    const serchParams = {
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc' as SortDirection,
+      filter: 'test'
+    };
+
+    const output = await controller.search(serchParams);
+    expect(mockListUseCase.execute).toBeCalledWith(serchParams);
+    expect(expectedOutput).toStrictEqual(output);
   });
 });
--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,54 +1,15 @@
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-import { CreateCategoryUseCase, DeleteCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase } from '@fc/micro-videos/category/application';
+import { CATEGORY_PROVIDERS } from './categories.providers';
+import { CategoriesService } from './categories.service';
 
-import { CategoryInMemoryRepository } from '@fc/micro-videos/category/infra';
-import { CategoryRepository } from '@fc/micro-videos/dist/category/domain';
 
 @Module({
   controllers: [CategoriesController],
   providers: [
     CategoriesService,
-    {
-      provide: 'CategoryRepository',
-      useClass: CategoryInMemoryRepository,
-    },
-    {
-      provide: CreateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new CreateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: UpdateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new UpdateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: GetCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new GetCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: DeleteCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new DeleteCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository'],
-    },
-    {
-      provide: ListCategoriesUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new ListCategoriesUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository'],
-    },
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
   ],
 })
 export class CategoriesModule { }
