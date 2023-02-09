6fd56dc1d35f74bb74aa15471887d0e8a4a50b4c
Renato <accounts01@softplayers.com>
2023-02-09 16:40:38 -0300

    chore: fix tests

--- src/nestjs/src/categories/categories.service.spec.ts
+++ src/nestjs/src/categories/categories.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
-
+import { CATEGORY_PROVIDERS } from './categories.providers';
 describe('CategoriesService', () => {
   let service: CategoriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+        ...Object.values(CATEGORY_PROVIDERS.USE_CASES),],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
